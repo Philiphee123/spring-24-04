006aaaa3ff76fdba049710f0ef5cc3d3f33ca9a2
Philiphee123 santos <felipe.santos1379@etec.sp.gov.br>
2024-04-24 13:30:00 +0000

    save.modelgenero

---
+++ app/src/main/java/application/repository/GeneroRepository.java
@@ -0,0 +1,9 @@
+package application.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface GeneroRepository extends CrudRepository<Genero, Long> {
+  
+    
+}
+
